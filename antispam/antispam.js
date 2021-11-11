5bff73e55a2f99abf437fe80ae4cc63112876f9c
psihozykt <psihozykt@gmail.com>
2021-11-12 02:24:00 +0900

    change moderating

--- antispam/antispam.js
+++ antispam/antispam.js
@@ -1,21 +1,21 @@
-let blockedWordsFile = require('./blocked_words');
-let blockedNamesFile = require('./blocked_names');
-let index = require('../index');
-let antispam = (channel, userstate, message, self) => {
-    if (self) return;
-    let chat = index.chat;
-    let client = index.client;
-    if (blockedWordsFile.some(r => message.toLowerCase().indexOf(r.messageText) >= 0)) {
-        client.timeout(channel, userstate.username, 1, "automatic timeout because of forbidden word")
-            .then(data => console.log(data))
-            .catch(err => console.log(err));
-    }
-    if (blockedNamesFile.some(r => userstate.username.toLowerCase().indexOf(r.username) >= 0)) {
-        client.timeout(channel, userstate.username, 1, "automatic timeout because of forbidden name")
-            .then(data => console.log(data))
-            .catch(err => console.log(err));
-    }
-
-
-};
-module.exports = antispam;
+// let blockedWordsFile = require('./blocked_words');
+// let blockedNamesFile = require('./blocked_names');
+// let index = require('../index');
+// let antispam = (channel, userstate, message, self) => {
+//     if (self) return;
+//     let chat = index.chat;
+//     let client = index.client;
+//     if (blockedWordsFile.some(r => message.toLowerCase().indexOf(r.messageText) >= 0)) {
+//         client.timeout(channel, userstate.username, 1, "automatic timeout because of forbidden word")
+//             .then(data => console.log(data))
+//             .catch(err => console.log(err));
+//     }
+//     if (blockedNamesFile.some(r => userstate.username.toLowerCase().indexOf(r.username) >= 0)) {
+//         client.timeout(channel, userstate.username, 1, "automatic timeout because of forbidden name")
+//             .then(data => console.log(data))
+//             .catch(err => console.log(err));
+//     }
+//
+//
+// };
+// module.exports = antispam;
