56693c6a1483513045e5443beee41b0b2a6efbe9
psihozykt <psihozykt@gmail.com>
2021-11-11 17:17:15 +0900

    moved from files to firebase

---
+++ commandsHandlerFileBackup.js
@@ -0,0 +1,157 @@
+const commandsFileHandler = require('./FileHandler')
+let h3lobby = require('./h3lobby/h3lobby')
+let getFollowAge = require('./twitchAPI/followAge');
+let getFollowAgeTop = require('./twitchAPI/followAgeTop')
+let banp = require('./antispam/banp')
+let banname = require('./antispam/banname')
+let getChannelPointsReward = require('./channelPoints/ariywariy/channelPoints')
+const Command = require('./command');
+let handlers = {
+     lobbyCommandsHandler:{
+        async handleCommand(message, channel, userstate) {
+            commandsFileHandler.setFileName(channel);
+            const words = message.toLowerCase().split(' ');
+            var commandRe = /!([\wа-яА-Я]+)/g;
+            let regResult = commandRe.exec(message);
+            let type = ""
+            if (regResult !== null) {
+                type = regResult[0];
+            }
+            switch (type) {
+                case("!rating"):
+                    return await h3lobby.getRating(channel, words).then(res => {
+                        return res;
+                    });
+                case("!stats"):
+                    return await h3lobby.getStats(channel, words).then(res => {
+                        return res;
+                    });
+                case("!last"):
+                    return await h3lobby.getLast(channel, words).then(res => {
+                        return res;
+                    });
+
+            }
+
+        },
+
+    },
+     basicCommandsHandler: {
+        async handleCommand(message, channel, userstate) {
+            commandsFileHandler.setFileName(channel);
+            const words = message.toLowerCase().split(' ');
+            var commandRe = /!([\wа-яА-Я]+)/g;
+            let regResult = commandRe.exec(message);
+            let type = ""
+            if (regResult !== null) {
+                type = regResult[0];
+            }
+            //Privelegues commands
+
+            if (userstate.mod || userstate.username === "psihoz_ykt") {
+                switch (type) {
+                    case("!add"):
+                        return this.addCommand(channel, message);
+                    case("!change"):
+                        return this.changeCommand(channel, message)
+                    case("!delete"):
+                        return this.deleteCommand(channel, message);
+                    case("!commands"):
+                        return this.getAllCommands(channel);
+                    default:
+                        if (commandsFileHandler.findCommandInFile(type)) {
+                            return this.findCommand(message);
+                        } else {
+                            return getChannelPointsReward(channel, userstate, message)
+                        }
+                }
+            } else {
+                switch (type) {
+                    default:
+                        if (commandsFileHandler.findCommandInFile(type)) {
+                            return this.findCommand(message);
+                        } else {
+                            return getChannelPointsReward(channel, userstate, message)
+                        }
+                }
+            }
+
+
+        },
+        //Input: Users message like "!add 111 Hello world!"
+        addCommand(channel, message) {
+            const command = new Command(message);
+            if (!command.text) return "";
+            if (commandsFileHandler.addCommandToFile(command))
+                return;
+            else
+                return command.getName() + " is already exists"
+
+        },
+        changeCommand
+            (channel, message) {
+            let messageCommand = new Command(message);
+            if (commandsFileHandler.isCommandAlreadyExists(messageCommand)) {
+                let command = commandsFileHandler.getCommandByName(messageCommand.getName());
+                commandsFileHandler.rewriteCommand(command, messageCommand.getText());
+                commandsFileHandler.updateFile();
+            } else {
+                return messageCommand.getName() + " does not exists"
+            }
+        }
+        ,
+        deleteCommand(channel, message) {
+            const command = new Command(message);
+            if (commandsFileHandler.isCommandAlreadyExists(command)) {
+                commandsFileHandler.deleteCommandFromFile(command)
+                commandsFileHandler.updateFile();
+                return command.getName() + " was deleted"
+            } else {
+                console.log('doesnt exists')
+                return command.getName() + " doesn't exists"
+            }
+        }
+        ,
+        getAllCommands(channel) {
+            return commandsFileHandler.getCommandsNames().join(" ")
+
+        },
+        findCommand(message) {
+            var commandRe = /!([\wа-яА-Я]+)/g;
+
+            let commandName = commandRe.exec(message)[0];
+            let username = "";
+            var re = /@(\w+)/g;
+            let regResult = re.exec(message);
+
+            if (regResult !== null) {
+                username = regResult[0];
+            }
+
+            let command = commandsFileHandler.findCommandInFile(commandName);
+            return username + " " + command.text
+        },
+    },
+    moderatingCommandsHandler: {
+        async handleCommand(message, channel, userstate) {
+            commandsFileHandler.setFileName(channel);
+            const words = message.toLowerCase().split(' ');
+            var commandRe = /!([\wа-яА-Я]+)/g;
+            let regResult = commandRe.exec(message);
+            let type = ""
+            if (regResult !== null) {
+                type = regResult[0];
+            }
+            if (userstate.mod || userstate.username === "psihoz_ykt" ) {
+                switch (type) {
+                    case ("!banp"):
+                        return banp(message, channel, userstate)
+                    case ("!banname"):
+                        return banname(message, channel, userstate)
+                }
+            }
+
+        },
+    },
+}
+module.exports = handlers
