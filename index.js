09d9fde9772a2559e90c280e3e8b6cb54180fa56
psihoz_ykt <psihozykt@gmail.com>
2021-12-09 06:22:59 +0900

    dsa

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = require('path')
 const http = require("http")
 const app = express()
 const server = http.createServer(app);
-const io = require('socket.io')();
+const io = require('socket.io')(server);
 
 if(process.env.NODE_ENV === "production")  {
 
