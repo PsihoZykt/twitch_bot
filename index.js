7ce619bd2684db8bfe59e6c2c65521e157600e0f
psihoz_ykt <psihozykt@gmail.com>
2021-12-09 07:55:11 +0900

    ds

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const server = createServer(app);
 const io = new Server(server
     , {
  // cors: {
-     transports: ['websocket'],
+     transports: ['websocket', "polling"],
  // },
  //    rejectUnauthorized: false ,
  //    allowEIO3: true,
